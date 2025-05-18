014045e9ea820df8ba1e10f65c82b5e63c73cb38
srk553 <96103382+srk553@users.noreply.github.com>
2025-05-18 08:31:22 +0530

    Create setup_tmux.sh

---
+++ setup_tmux.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+## Add this to your ~/.tmux.conf:
+
+# set -g @plugin 'tmux-plugins/tmux-resurrect'
+# run '~/.tmux/plugins/tpm/tpm'
+
+
+# run tmux source ~/.tmux.conf
+# Save layout: Prefix + Ctrl-s
+# Restore layout: Prefix + Ctrl-r
